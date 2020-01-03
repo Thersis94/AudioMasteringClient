d85c901b5f30a402c97d09fb8704dbe9e9fbe714
Thersis94 <thersis94@gmail.com>
2020-01-02 18:19:34 -0700

    adding another heatmap

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,12 +6,13 @@ export default class HomePage extends Component {
   render() {
     return (
       <span className="home-page-app-description">
+        <span className='waveform-background'>
         <h2 className="home-page-title">How does it work?</h2>
-        <img
+        {/* <img
           className="heatmap-img"
           src="https://raw.githubusercontent.com/Thersis94/AudioMasteringClient/master/Pictures/Heatmap2.PNG"
           alt="Picture demonstrating a heatmap."
-        />
+        /> */}
         <p className="mastering-explanation">
           When you upload a audio file a heatmap is created. The heatmap is a
           representation of all of the frequencies and their volume at any given
@@ -19,8 +20,9 @@ export default class HomePage extends Component {
           Stereo polarization, and Limiting to match the heatmap of your song to
           the average heatmaps of thousands of hit songs.
         </p>
+        </span>
         <section>
-          <h2>Compatible File Types</h2>
+          <h2 className='file-types-header'>Compatible File Types</h2>
           <span className="file-types-lists">
             <section>
               <h3 className='file-type-header'>WAV</h3>
