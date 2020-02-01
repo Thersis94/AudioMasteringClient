2e1a1b2ebfafc75e39870004993a3f46acb97ce0
Thersis94 <thersis94@gmail.com>
2020-02-01 13:20:22 -0700

    Adding more information to the info section.

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -6,8 +6,19 @@ export default class HomePage extends Component {
     render() {
       return (
         <span className="home-page-app-description">
-          <h2 className="home-page-title">How does it work?</h2>
           <section>
+            <h2 className='mission-header'>Our Mission</h2>
+            <p className='mission-text'>
+                AI Audio strives to make mastering services accessable to musicians by using AI to master their songs.
+            </p>
+          </section>
+          <h2 className="home-page-title">What exactly is mastering?</h2>
+          <section>
+            <div>
+              <p className='what-is-mastering-text'>
+                Mastering is the final process that music undergoes before being published. We use mastering to ensure that your songs sound the same in every listening environment. In short, mastering takes WAV files and equalizes, compresses, limits, and enhances the sound across different environments.
+              </p>
+            </div>
           <div className="waveform-and-description">
           <img
             className="waveform-img"
@@ -27,12 +38,18 @@ export default class HomePage extends Component {
           <p className="mastering-explanation">
             When you upload a audio file a heatmap is created. The heatmap is a
             representation of all of the frequencies and their volume at any given
-            point in the song. The AI then uses a combination of Equalization,
-            Stereo Polarization, and Limiting to match the heatmap of your song to
-            the average heatmaps of thousands of industry standard songs.
+            point in the song.
           </p>
           </div>
-          </section>      
+          </section>
+          <section>
+            <h3 className='how-it-works-header'>How it works</h3>
+            <p className='how-it-works-text'>
+            AI Audio uses a combination of Equalization,
+            Stereo Polarization, and Limiting to match the heatmap of your song to
+            the average heatmaps of thousands of industry standard songs.
+            </p>
+          </section>
           <section>
             <h2 className='file-types-header'>Compatible File Types</h2>
             <span className="file-types-lists">
